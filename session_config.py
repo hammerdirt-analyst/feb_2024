32068f6575c727501dc33a7ad288ba0db24b91d6
roger@hammerdirt.ch <roger@hammerdirt.ch>
2024-04-29 12:46:10 +0200

    edited the annex chapters according to new requirements

---
+++ session_config.py
@@ -0,0 +1,185 @@
+"""
+Parameters for the session configuration
+
+"""
+import pandas as pd
+
+# labels and groups for selection
+administrative = ['location', 'city', 'canton', 'parent_boundary']
+geographic = ['feature_name',  'feature_type']
+feature_types = ['r', 'l', 'p']
+feature_variables = ['orchards', 'vineyards', 'buildings', 'forest', 'undefined', 'public services', 'streets']
+the_report_themes = ['Canton', 'Municipality', 'River basin', 'River', 'Lake', 'Park']
+
+# the consolidated codes
+gfoam_frags_caps = {
+    'Gfoams': ['G81', 'G82', 'G83'],
+    'Gfrags': ['G78', 'G79', 'G80', 'G75', 'G76', 'G77'],
+    'Gcaps': ['G21', 'G22', 'G23', 'G24'],
+}
+
+period_dates = {
+    "mcbp":["2015-11-15", "2016-12-01"],
+    "slr": ["2017-04-01", "2018-05-01"],
+    "iqaasl": ["2020-03-01", "2021-05-31"],
+    "plastock":["2022-01-01", "2022-12-31"]
+}
+
+feature_data = dict(
+    code_data = "data/end_process/codes.csv",
+    beach_data = "data/end_process/beaches.csv",
+    land_cover_data = "data/end_process/land_cover.csv",
+    land_use_data = "data/end_process/land_use.csv",
+    street_data = "data/end_process/streets.csv",
+    intersection_attributes = "data/end_process/river_intersect_lakes.csv"
+
+)
+
+
+
+
+
+
+
+# languages
+languages = ['de', 'en', 'fr']
+
+session_language = 'en'
+
+# quintiles for the report
+report_quantiles = [.05, .25, .5, .75, .95]
+
+# bins and labels for land use categorization
+bins = [-1, 0.2, 0.4, 0.6, 0.8, 1]
+bin_labels = [1, 2, 3, 4, 5]
+
+# area of a buffer pi*1500²
+buffer_radius = 1500
+buffer_area = 7068583
+
+default_survey_type = 'length'
+
+# column of interest
+object_of_interest = 'code'
+
+# indentify target variable and type
+Y = 'pcs/m'
+y_type = 'float'
+
+Q = 'quantity'
+
+# distribution point estimate
+tendencies = 'mean'
+
+# correlation threshold
+corr_threshold = 0.3
+
+# these are the columns and methods used to aggregate the data at the sample level
+# the sample level is the lowest level of aggregation. The sample level is the collection of all
+# the records that share the same sample_id or loc_date. The loc_date is the unique identifier for each survey.
+unit_agg = {
+    Q: "sum",
+    Y: "sum"
+}
+
+# Once the data is aggregated at the sample level, it is aggregated at the feature level. The pcs/m or pcs_m
+# column can no longer be summed. We can only talk about the median, average or distribution of the pcs/m for the
+# samples contained in each feature. The quantity column can still be summed. The median is used for reporting
+# purposes. The median is less sensitive to outliers than the average. The median is also more intuitive than the
+# average.
+
+agg_groups = {
+    Q: "sum",
+    Y: tendencies
+}
+
+# this column is used to key feature variables to 'Y'
+location_label = "location"
+
+# the sample id
+index_label = "sample_id"
+
+# default location of data
+data_directory = 'data/end_process/'
+date_format = "%Y-%m-%d"
+default_data = ['data/end_process/before_2019.csv', 'data/end_process/after_2019.csv']
+
+
+def assign_target_variable(target: str = None) -> str:
+    if target is None:
+        return Y
+    if target not in [Y, 'pcs/m2']:
+        raise ValueError('target must be either pcs/m or pcs/m2')
+    return target
+
+
+
+
+def define_data_fields(survey_type: str = None, reporting_units: str = None) -> []:
+
+    if survey_type is None:
+        survey_type = default_survey_type
+    if reporting_units is None:
+        reporting_units = Y
+
+    if survey_type not in [default_survey_type, 'area']:
+        raise ValueError('survey_type must be either length or area')
+    if reporting_units not in [Y, 'pcs/m2']:
+        raise ValueError('reporting_units must be either pcs/m or pcs/m2')
+
+    columns = [
+        'sample_id',
+        'date',
+        'location',
+        survey_type,
+        'code',
+        'quantity',
+        reporting_units,
+        'feature_name',
+        'feature_type',
+        'parent_boundary',
+        'city',
+        'canton',
+        'en',
+        'fr',
+        'de',
+        'date']
+    return columns
+
+def collect_survey_data(connection: str = None, afunc: callable = None, columns: [] = None) -> pd.DataFrame:
+    if connection is None:
+        datas = [pd.read_csv(data) for data in default_data]
+        return pd.concat(datas)
+    if connection == 'external':
+        # this function should return a dataframe
+        datas = afunc()
+        if columns is None:
+            if all(col in define_data_fields() for col in datas.columns):
+                return datas
+            else:
+                raise ValueError(f'The columns in the dataframe : {datas.columns} do not match the default columns : {define_data_fields()}')
+        else:
+            if all(col in columns for col in datas.columns):
+                return datas
+            else:
+                raise ValueError(f'The columns in the dataframe : {datas.columns} do not match the supplied columns {columns}')
+
+
+def select_features(df, column, labels):
+    feature_mask = df[column].isin(labels)
+    return feature_mask
+
+def select_dates(df, start_end):
+    data_mask = (df['date'] >= start_end[0]) & (df['date'] <= start_end[1])
+    return data_mask
+def filter_data(df: pd.DataFrame, date_mask: pd.Series = None, feature_mask: pd.Series = None) -> pd.DataFrame:
+    if date_mask is None and feature_mask is None:
+        return df
+    elif date_mask is None and feature_mask is not None:
+        mask = feature_mask
+    elif date_mask is not None and feature_mask is None:
+        mask = date_mask
+    else:
+        mask = date_mask & feature_mask
+
+    return df[mask]
